a1052b46960272b9512311fb1d0dc58c4ed6f7d1
Isa_oliveira <belefreiresls@outloo.com>
2022-10-25 19:35:42 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'API-ANGULAR-VERSION'`, () => {
+  it(`should have as title 'consumindoApi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('API-ANGULAR-VERSION');
+    expect(app.title).toEqual('IlustrationAPI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('API-ANGULAR-VERSION app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Ilustration API app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { ClientesService } from './services/clientes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'API-ANGULAR-VERSION';
+export class AppComponent implements OnInit{
+  public clientes$: any = []
+  title: any;
+  constructor(private clientesServices: ClientesService){
+    this.getClientes()
+  }
+  ngOnInit(): void {
+    this.getClientes()
+  }
+  getClientes(){
+    this.clientesServices.getAll().subscribe((clientes) => (this.clientes$ = clientes))
+  }
+  cadastrarClientes(){
+    let nome = document.getElementById("nome") as HTMLInputElement;
+    let cliente = {
+      nome: nome.value
+    }
+    this.clientesServices.save(cliente).subscribe()
+  }
 }
