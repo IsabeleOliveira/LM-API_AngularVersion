774fb60c418c6af4dc0ed9a10b47b6389a8bf50a
Isa_oliveira <belefreiresls@outloo.com>
2022-10-26 22:36:59 -0300

    Aplicando o Dell Design System

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,6 @@ import { ClientesService } from './services/clientes.service';
 })
 export class AppComponent implements OnInit{
   public clientes$: any = []
-  title: any;
   constructor(private clientesServices: ClientesService){
     this.getClientes()
   }
@@ -21,10 +20,15 @@ export class AppComponent implements OnInit{
     this.clientesServices.getAll().subscribe((clientes) => (this.clientes$ = clientes))
   }
   cadastrarClientes(){
-    let nome = document.getElementById("nome") as HTMLInputElement;
-    let cliente = {
+    let nome = document.getElementById("nomeinput") as HTMLInputElement;
+    if(nome.value =="" || nome ==null){
+      
+    }else{
+      let cliente = {
       nome: nome.value
-    }
+      }
     this.clientesServices.save(cliente).subscribe()
+    }
   }
+
 }
